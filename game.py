14663122a0a0e03edf55d3c90848c5755ec329b5
kris10s7 <50898383+kris10s7@users.noreply.github.com>
2019-06-14 21:32:33 -0400

    Determining Winning Option

--- game.py
+++ game.py
@@ -18,11 +18,33 @@ else:
 
 # GENERATE COMPUTER SELECTION
 
-print("GENERATING...")
+
 
 computer_choice = random.choice(["rock", "paper", "scissors"])
 print("----------")
+print("GENERATING...")
 print("COMPUTER CHOICE:", computer_choice)
 # DETERMINE THE WINNER
+
+# rock beats scissors
+# paper beats rock
+# scissors beats paper
+# same selections is a tie
+#
+if user_choices == computer_choice:
+    print("TIE")
+elif user_choices == "rock" and computer_choice == "paper":
+    print("PAPER")
+elif user_choices == "paper" and computer_choice == "scissors":
+    print("SCISSORS")
+elif user_choices == "scissors" and computer_choice == "rock":
+    print("ROCK")
+elif user_choices == "rock" and computer_choice == "scissors":
+    print("ROCK")
+elif user_choices == "paper" and computer_choice == "rock":
+    print("PAPER")
+elif user_choices == "scissors" and computer_choice == "paper":
+    print("SCISSORS")
+
 
 # DISPLAY FINAL OUTPUTS / OUTCOMES
